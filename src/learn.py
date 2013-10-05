da04358e6fdf307637fdac37ecf817a57e9505aa
Andrew Jones <me@andrewhjon.es>
2013-10-05 19:21:47 -0400

    Refactor to make the load interface cleaner

--- src/learn.py
+++ src/learn.py
@@ -2,15 +2,20 @@ import csv
 import codecs
 import pandas as pd
 
-def unicode_csv_reader(unicode_csv_data):
-    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data))
-    for row in csv_reader:
-        yield [unicode(cell, 'utf-8') for cell in row]
+def load_data_frame(path):
 
-def utf_8_encoder(unicode_csv_data):
-    for line in unicode_csv_data:
-        yield line.encode('utf-8')
+    def unicode_csv_reader(unicode_csv_data):
+        csv_reader = csv.reader(utf_8_encoder(unicode_csv_data))
+        for row in csv_reader:
+            yield [unicode(cell, 'utf-8') for cell in row]
 
-file = unicode_csv_reader(codecs.open('../data/train-utf8.csv', 'r', 'utf-8'))
-columns = next(file)
-df = pd.DataFrame.from_records([x for x in file], columns=columns)
+    def utf_8_encoder(unicode_csv_data):
+        for line in unicode_csv_data:
+            yield line.encode('utf-8')
+
+    file = unicode_csv_reader(codecs.open(path, 'r', 'utf-8'))
+
+    columns = next(file)
+    return pd.DataFrame.from_records([x for x in file], columns=columns)
+
+insults = load_data_frame('../data/train-utf8.csv')
